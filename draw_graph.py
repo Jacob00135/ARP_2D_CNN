6582138f897d4e2ec70e15a98d1a39a5292aa31f
Jacob00135 <2428207444@qq.com>
2024-06-14 06:02:01 +0800

    在画图之前，剔除密集的点（不包括最优点）

--- draw_graph.py
+++ draw_graph.py
@@ -25,18 +25,40 @@ def draw_full_plot(x, y, xlabel, save_path):
     plt.xlabel(xlabel)
     plt.ylabel('benefit')
     plt.grid(True, c='#eeeeee', ls='--', lw=0.5, zorder=0)
-    # ax.set_aspect('equal', adjustable='box')
+    ax.set_aspect('equal', adjustable='box')
     # fig.set_size_inches(3.5, 3.5)
     # plt.subplots_adjust(left=0.15, bottom=0.13, top=0.93, right=0.95)
     plt.savefig(save_path)
     plt.close()
 
 
+def filtrate_dense_point(x, y, x_threshold=0.01, y_threshold=0.01, include_best_point=False):
+    if x.shape[0] != y.shape[0]:
+        raise ValueError('x和y长度不一致！')
+
+    length = x.shape[0]
+    filtrate_boolean = np.ones(length, dtype='bool')
+    for i in range(length - 2):
+        for j in range(i + 1, length):
+            if abs(x[j] - x[i]) <= x_threshold and abs(y[j] - y[i]) <= y_threshold:
+                filtrate_boolean[j] = False
+
+    if not include_best_point:
+        filtrate_boolean[x.argmax()] = True
+        filtrate_boolean[y.argmax()] = True
+
+    x = x[filtrate_boolean]
+    y = y[filtrate_boolean]
+
+    return x, y
+
+
 if __name__ == '__main__':
-    model_name = 'xjy_20240611'
+    model_name = 'xjy_20240613'
     data = pd.read_csv(os.path.join(root_path, 'eval_result/{}/performance.csv'.format(model_name)))
     y = data['benefit'].values
     for y_name in ['accuracy', 'precision', 'recall', 'f1', 'auc', 'ap']:
         x = data[y_name].values
+        filtered_x, filtered_y = filtrate_dense_point(x, y)
         save_path = os.path.join(root_path, 'eval_result/{}/{}.png'.format(model_name, y_name))
-        draw_full_plot(x, y, y_name, save_path)
+        draw_full_plot(filtered_x, filtered_y, y_name, save_path)
